ab36ef408677a25482c50d19a2dbdeb226f59968
LTKort <lars.kort94@gmail.com>
2022-05-03 15:43:09 +0200

    nova 4 init

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,9 @@
 let mix = require("laravel-mix");
+let path = require("path");
 
-mix.js("resources/js/field.js", "dist/js/nova-fontawesome.js").vue();
+require("./mix");
+
+mix.setPublicPath("dist")
+    .js("resources/js/field.js", "dist/js/nova-fontawesome.js")
+    .vue({ version: 3 })
+    .nova("marshmallow/nova-fontawesome");
